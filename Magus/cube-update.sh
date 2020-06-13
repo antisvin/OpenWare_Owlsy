9a2f594f23212c4a7a8700d24e5769222c284892
Stanislav Shtin <antisvin@gmail.com>
2020-06-13 17:39:19 +0300

    Cube update

--- Magus/cube-update.sh
+++ Magus/cube-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git checkout Inc/usbh_conf.h
 git checkout Src/usb_device.c Src/usb_host.c Makefile
+git checkout Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c
 rm -f Src/usbd_audio_if.c Inc/usbd_audio_if.h
