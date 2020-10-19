4c9be1d108c5c4348fd3bdaf5500c55f35749464
antisvin <antisvin@gmail.com>
2020-10-19 20:08:38 +0300

    Fix for OTG FS interrupt

--- MidiBootDaisy/Inc/hardware.h
+++ MidiBootDaisy/Inc/hardware.h
@@ -4,7 +4,8 @@
 #define USE_USBD_MIDI
 #define USBD_MAX_POWER              100 // 100mA for iPad compatibility
 #define USBD_HANDLE hUsbDeviceFS
-#define BOOTLOADER_MODE
+
+#define MAX_SYSEX_FIRMWARE_SIZE 512 * 1024
 
 #if defined DAISY
   #define HARDWARE_VERSION    "Daisy Boot"
@@ -20,10 +21,10 @@
 #define FIRMWARE_HEADER        0xBABECAFE
 #define FIRMWARE_RELOCATIONS_COUNT 5
 
-#ifndef DEBUG
 #define USE_IWDG
-#endif
 
-#define USE_ENCODER_PIN__
+#if 0
+#define USE_ENCODER_PIN
 #define ENC_CLICK_Pin GPIO_PIN_12
 #define ENC_CLICK_GPIO_Port GPIOB
+#endif
