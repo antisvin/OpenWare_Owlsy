916e90badae6c24406a023e7e29ebe607ebb6a8d
antisvin <antisvin@gmail.com>
2021-04-16 23:12:22 +0300

    Missing file added

---
+++ Hardware/f1-libs.mk
@@ -0,0 +1,19 @@
+# object files
+
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_rcc.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_dma.c
+# C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_dma_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_gpio.c
+# C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_adc.c
+# C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_adc_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_pwr.c
+#C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_pwr_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_rcc_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_flash.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_flash_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_tim_ex.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_tim.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_cortex.c
+ C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_uart.c
+C_SRC += $(DRIVERS)/Src/stm32f1xx_hal_spi.c
