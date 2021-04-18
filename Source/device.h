bf43389ab26fdee46d521f1c7408b05fda2c2d0a
antisvin <antisvin@gmail.com>
2021-04-18 15:57:07 +0300

    Version 22.1

--- Source/device.h
+++ Source/device.h
@@ -3,8 +3,8 @@
 
 #include "hardware.h"
 
-#define FIRMWARE_VERSION_MAJOR       23
-#define FIRMWARE_VERSION_MINOR       0
+#define FIRMWARE_VERSION_MAJOR       22
+#define FIRMWARE_VERSION_MINOR       1
 #define _FW_STR(x)                   #x
 #define _FW_TO_STR(x)                _FW_STR(x)
 #define FIRMWARE_VERSION             "v" _FW_TO_STR(FIRMWARE_VERSION_MAJOR) "." _FW_TO_STR(FIRMWARE_VERSION_MINOR)
