69b21f5e9ef341d29e709cedf414d144a52dfe23
antisvin <antisvin@gmail.com>
2022-08-05 19:42:10 +0300

    Patch.init added

---
+++ PatchInit/Inc/hardware.h
@@ -0,0 +1,87 @@
+#include "main.h"
+#include "stm32h7xx_hal.h"
+
+#define USBD_HANDLE hUsbDeviceFS
+
+#if defined PATCH_INIT
+  #define HARDWARE_ID                  PATCH_INIT_HARDWARE
+  #define HARDWARE_VERSION             "Owlsy"
+#else
+  #error Invalid configuration
+#endif
+
+#define USE_FIRMWARE_HEADER
+#define FLASH_TASK_STACK_SIZE        (1024/sizeof(portSTACK_TYPE))
+
+//#define PROGRAM_TASK_STACK_SIZE      (6*1024/sizeof(portSTACK_TYPE))
+//#define UTILITY_TASK_STACK_SIZE      (1024/sizeof(portSTACK_TYPE))
+//#define MANAGER_TASK_STACK_SIZE      (2048/sizeof(portSTACK_TYPE))
+
+//#define MAIN_LOOP_SLEEP_MS          20
+
+//#define MINIMAL_BUILD
+
+// FW size limit would matter only if we use bootloader, otherwise linker scripts sets this limit
+#define MAX_SYSEX_FIRMWARE_SIZE 512 * 1024
+//#define MAX_SYSEX_PROGRAM_SIZE 510 * 1024
+// Program size is limited by patch RAM section size
+//#define MAX_SYSEX_PROGRAM_SIZE 448 * 1024
+//#define MAX_SYSEX_PROGRAM_SIZE 256 * 1024
+
+#define MAX_NUMBER_OF_PATCHES       40
+#define MAX_NUMBER_OF_RESOURCES     60
+#define STORAGE_MAX_BLOCKS          128
+
+#define USE_DAC
+#define DAC_PERIPH hdac1
+#define USE_ADC
+#define ADC_PERIPH hadc1
+#define ADC_A 0
+#define ADC_B 1
+#define ADC_C 2
+#define ADC_D 3
+#define NOF_ADC_VALUES               4
+#define NOF_PARAMETERS               40
+#define NOF_BUTTONS                  (4 + 2)
+
+#define LOAD_INDICATOR_PARAMETER     PARAMETER_E
+// Parameters A-D are used by ADC, so we can't set values for them
+
+#define USE_EXTERNAL_RAM
+#define USE_PLUS_RAM
+#define USE_CACHE
+
+#define USE_USBD_MIDI
+#define USE_USBD_AUDIO
+#define USE_USBD_AUDIO_TX  // microphone
+//#define USE_USBD_AUDIO_RX // speaker
+/* USB audio settings */
+#define AUDIO_BITS_PER_SAMPLE       16
+#define AUDIO_BYTES_PER_SAMPLE      (AUDIO_BITS_PER_SAMPLE/8)
+/* #define AUDIO_INT32_TO_SAMPLE(x)    (__REV16((x)>>8)) */
+/* #define AUDIO_SAMPLE_TO_INT32(x)    ((int32_t)(__REV16(x))<<8) */
+#define AUDIO_INT32_TO_SAMPLE(x)    ((x)>>8)
+#define AUDIO_SAMPLE_TO_INT32(x)    ((int32_t)(x)<<8)
+
+
+#define USE_CODEC
+#define DUAL_CODEC
+#define USE_PCM3060
+#define USE_USBD_FS
+#define USBD_HANDLE hUsbDeviceFS
+#define AUDIO_BITS_PER_SAMPLE       16
+#define AUDIO_BYTES_PER_SAMPLE      (AUDIO_BITS_PER_SAMPLE/8)
+#define AUDIO_CHANNELS              2
+
+#define QSPI_HANDLE hqspi
+//#define QSPI_DEVICE_IS25LP080D
+#define QSPI_DEVICE_IS25LP064A
+
+#define USE_IWDG
+#define IWDG_PERIPH IWDG1
+/* #define INIT_FMC */
+
+#define USE_FATFS
+#define FATFS_USE_DMA
+#define FATFS_SD_HANDLE hsd1
+
---
+++ PatchInit/Src/PatchInit.cpp
@@ -0,0 +1,112 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "device.h"
+#include "Owl.h"
+#include "Graphics.h"
+//#include "errorhandlers.h"
+#include "message.h"
+#include "gpio.h"
+#include "qspicontrol.h"
+#include "ProgramManager.h"
+#include "PatchRegistry.h"
+#include "OpenWareMidiControl.h"
+#include "SoftwareEncoder.hpp"
+#include "VersionToken.h"
+
+extern bool updateUi;
+extern uint16_t button_values;
+
+void setGateValue(uint8_t bid, int16_t value){
+  if (bid == BUTTON_C || bid == PUSHBUTTON) {
+    HAL_GPIO_WritePin(GATE_OUT_GPIO_Port, GATE_OUT_Pin, value ? GPIO_PIN_SET :  GPIO_PIN_RESET);
+    // We have to update button values here, because currently it's not propagated from program vector. A bug?
+    // setButtonValue(BUTTON_C, value);
+    //button_values &= ~((!value) << BUTTON_C);
+    //button_values |= (bool(value) << BUTTON_C);
+  }
+}
+
+void setAnalogValue(uint8_t ch, int16_t value){
+  extern DAC_HandleTypeDef DAC_PERIPH;
+  switch(ch){
+  case PARAMETER_F:
+    HAL_DAC_SetValue(&DAC_PERIPH, DAC_CHANNEL_1, DAC_ALIGN_12B_R, __USAT(value, 12));
+    break;
+  case PARAMETER_G:
+    HAL_DAC_SetValue(&DAC_PERIPH, DAC_CHANNEL_2, DAC_ALIGN_12B_R, __USAT(value, 12));
+    break;
+  }
+}
+
+void updateParameters(int16_t* parameter_values, size_t parameter_len, uint16_t* adc_values, size_t adc_len){
+#ifdef USE_CACHE
+  //SCB_InvalidateDCache_by_Addr((uint32_t*)adc_values, sizeof(adc_values));
+#endif
+  // Note that updateValue will apply smoothing, so we don't have to do it here
+  graphics.params.updateValue(0, 4095 - adc_values[0]);
+  graphics.params.updateValue(1, 4095 - adc_values[1]);
+  graphics.params.updateValue(2, 4095 - adc_values[2]);
+  graphics.params.updateValue(3, 4095 - adc_values[3]);
+}
+
+void onChangePin(uint16_t pin){
+  switch(pin){
+  case GATE_IN1_Pin: {
+    bool state = !(GATE_IN1_GPIO_Port->IDR & GATE_IN1_Pin);
+    setButtonValue(BUTTON_A, state);
+    setButtonValue(PUSHBUTTON, state);
+    break;
+  }
+  case GATE_IN2_Pin:
+    setButtonValue(BUTTON_B, !(GATE_IN2_GPIO_Port->IDR & GATE_IN2_Pin));
+    break;
+  default:
+    break;
+  }
+}
+
+void setup(){
+  HAL_GPIO_WritePin(OLED_RST_GPIO_Port, OLED_RST_Pin, GPIO_PIN_RESET); // OLED off
+
+  qspi_init(QSPI_MODE_MEMORY_MAPPED);
+  
+  /* This doesn't work with bootloader, need to find how to deinit it earlier*/
+  #if 0
+  if (qspi_init(QSPI_MODE_MEMORY_MAPPED) != MEMORY_OK){
+    // We can end here only if QSPI settings are misconfigured
+    error(RUNTIME_ERROR, "Flash init error");
+  }
+  #endif
+
+  extern SPI_HandleTypeDef OLED_SPI;
+  graphics.begin(&OLED_SPI);
+
+  owl.setup();
+  setButtonValue(PUSHBUTTON, 0);
+}
+
+static int16_t enc_data[2];
+
+void loop(){
+  updateEncoderCounter();
+  if (updateUi){
+#if defined USE_CACHE
+    SCB_CleanInvalidateDCache_by_Addr((uint32_t*)graphics.params.user, sizeof(graphics.params.user));
+#endif
+    encoder.debounce();
+    enc_data[0] = int16_t(
+      encoder.isRisingEdge() | (encoder.isFallingEdge() << 1) | (encoder.isLongPress() << 2));
+    enc_data[1] = int16_t(encoder.getValue());
+    graphics.params.updateEncoders(enc_data, 2);
+
+    for(int i = NOF_ADC_VALUES; i < NOF_PARAMETERS; ++i) {
+      graphics.params.updateValue(i, 0);
+    }
+
+    graphics.draw();
+    graphics.display();
+  }
+  
+  owl.loop();
+}
